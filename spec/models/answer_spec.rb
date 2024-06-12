e086a69b41e4eed13bd600239444e2bc8163390d
Tom Crouch <tom.crouch@gmail.com>
2024-06-12 17:42:02 +0100

    Add model specs

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Answer do
+  it 'has a valid factory' do
+    expect(build(:answer)).to be_valid
+  end
+
+  describe 'validations' do
+    subject { build(:answer) }
+
+    it { is_expected.to validate_presence_of(:text) }
+  end
+end
---
+++ spec/models/question_statistic_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe QuestionStatistic do
+  it 'has a valid factory' do
+    expect(build(:question_statistic)).to be_valid
+  end
+end
