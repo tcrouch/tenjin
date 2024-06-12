652af5b5e823f9d2fc7a364e80d1185cf393cd30
Tom Crouch <t.crouch@outwood.com>
2024-06-12 08:56:03 +0100

    Fix customisation factory

--- spec/factories/customisations.rb
+++ spec/factories/customisations.rb
@@ -5,11 +5,12 @@ FactoryBot.define do
     customisation_type { 'leaderboard_icon' }
     cost { rand(0..10) }
     sequence(:name) { |n| "#{FFaker::Lorem.word} #{n}" }
-    sequence(:value) { |n| "#{FFaker::Lorem.word} #{n}" }
+    sequence(:value) { "#{FFaker::Color.name},heart" }
     purchasable { true }
 
     factory :dashboard_customisation do
       customisation_type { 'dashboard_style' }
+      sequence(:value) { FFaker::Color.name }
 
       after(:build) do |c|
         c.image.attach(io: Rails.root.join('spec/fixtures/files/game-pieces.jpg').open,
