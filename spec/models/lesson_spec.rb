033cc45263847ddfd2e119dbaf2388c0190bddb6
Tom Crouch <tom.crouch@gmail.com>
2024-06-12 17:41:00 +0100

    Add validation

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -4,4 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Lesson do
   it { is_expected.to have_many(:questions) }
+
+  describe 'validations' do
+    it { is_expected.to validate_length_of(:title).is_at_least(3) }
+  end
 end
