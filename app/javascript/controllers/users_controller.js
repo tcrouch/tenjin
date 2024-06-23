a3533259897394775eb85dbcf6a04a31ed9fce74
Tom Crouch <tom.crouch@gmail.com>
2024-06-23 23:00:08 +0100

    eslint corrections

--- app/javascript/controllers/users_controller.js
+++ app/javascript/controllers/users_controller.js
@@ -1,26 +1,26 @@
 // src/controllers/users_controller.js
-import { Controller } from 'stimulus'
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  initialize () {
-    if (document.getElementById('loginGoogle')) {
-      const tour = new Shepherd.Tour({ useModalOverlay: true })
+  initialize() {
+    if (document.getElementById("loginGoogle")) {
+      const tour = new Shepherd.Tour({ useModalOverlay: true });
       tour.addStep({
-        id: 'Click Login Google',
+        id: "Click Login Google",
         text: "Click on this button, login and you're done!",
         attachTo: {
-          element: '#loginGoogle',
-          on: 'bottom'
+          element: "#loginGoogle",
+          on: "bottom",
         },
         buttons: [
           {
-            text: 'Later',
-            action: tour.cancel
-          }
-        ]
-      })
+            text: "Later",
+            action: tour.cancel,
+          },
+        ],
+      });
 
-      tour.start()
+      tour.start();
     }
   }
 }
