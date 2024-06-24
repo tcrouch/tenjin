388f032d81d72b3ae00ddbe6e0df152fe2ff1555
Tom Crouch <tom.crouch@gmail.com>
2024-06-24 22:26:05 +0100

    Update webpack config

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,32 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
+const { generateWebpackConfig, merge, web } = require("shakapacker");
+const webpack = require("webpack");
+const webpackConfig = generateWebpackConfig();
 
-const webpackConfig = generateWebpackConfig()
+const options = {
+  resolve: {
+    extensions: [".css", ".scss", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      Popper: ["popper.js", "default"],
+      Rails: ["@rails/ujs"],
+    }),
+  ],
+};
 
-module.exports = webpackConfig
+module.exports = merge(options, webpackConfig);
