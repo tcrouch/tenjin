5465277a4c382717e8e2ba53a22918fbd531fa19
Tom Crouch <tom.crouch@gmail.com>
2026-05-30 13:26:53 +0100

    style(js): prettier format google_analytics.js

--- app/javascript/app/google_analytics.js
+++ app/javascript/app/google_analytics.js
@@ -1,7 +1,8 @@
 document.addEventListener("turbolinks:load", function (event) {
   if (typeof gtag === "function") {
-    const gaId = document.querySelector('meta[name="google-analytics-id"]')
-      ?.content;
+    const gaId = document.querySelector(
+      'meta[name="google-analytics-id"]',
+    )?.content;
     if (gaId) {
       gtag("config", gaId, {
         page_location: event.data.url,
