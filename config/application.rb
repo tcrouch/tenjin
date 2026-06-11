d2f76624f65f46822804b0149c14903442756f61
Tom Crouch <tom.crouch@gmail.com>
2026-06-11 18:43:43 +0100

    chore: load Rails 7.1 framework defaults

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module Csquiz
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.active_storage.variant_processor = :mini_magick
     config.active_support.cache_format_version = 7.1
