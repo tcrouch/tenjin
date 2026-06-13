db83e08ac7d2afaba4e6ba40212217f4073d3318
Tom Crouch <tom.crouch@gmail.com>
2026-06-13 16:29:42 +0100

    chore(routes): add empty system namespace block

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {invitations: "admins/invitations"}
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 
+  namespace :system do
+    # Resources will be added here phase by phase.
+  end
+
   resources :quizzes
   resources :schools do
     collection do
