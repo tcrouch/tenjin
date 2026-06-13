0df284e9bfdb05f290a7e5385e2628a7df6e5a68
Tom Crouch <tom.crouch@gmail.com>
2026-06-13 20:56:14 +0100

    refactor(devise): move admin invitations controller under System::

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {invitations: "admins/invitations"}
+  devise_for :admins, controllers: {invitations: "system/invitations"}
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 
   namespace :system do
